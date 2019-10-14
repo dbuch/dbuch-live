d69224d4b0e18a1c0732161d03fa86a9cad0c020
Daniel Buch Hansen <boogiewasthere@gmail.com>
2019-10-14 13:16:15 +0200

    cleanup

--- build.sh
+++ build.sh
@@ -5,8 +5,8 @@ set -e -u
 iso_name=dbuch-live
 iso_label="dbuch-live-$(date +%Y%m)"
 iso_publisher="Daniel Buch Hansen <https://www.github.com/dbuch>"
-iso_application="dbuch live iso"
-iso_version=$(date +%Y.%m.%d)
+iso_application="dbuch Live iso"
+iso_version=$(date +%Y.%m)
 install_dir=arch
 work_dir=work
 out_dir=out
@@ -16,32 +16,6 @@ verbose="-v"
 script_path=$(readlink -f ${0%/*})
 
 umask 0022
-
-_usage ()
-{
-    echo "usage ${0} [options]"
-    echo
-    echo " General options:"
-    echo "    -N <iso_name>      Set an iso filename (prefix)"
-    echo "                        Default: ${iso_name}"
-    echo "    -V <iso_version>   Set an iso version (in filename)"
-    echo "                        Default: ${iso_version}"
-    echo "    -L <iso_label>     Set an iso label (disk label)"
-    echo "                        Default: ${iso_label}"
-    echo "    -P <publisher>     Set a publisher for the disk"
-    echo "                        Default: '${iso_publisher}'"
-    echo "    -A <application>   Set an application name for the disk"
-    echo "                        Default: '${iso_application}'"
-    echo "    -D <install_dir>   Set an install_dir (directory inside iso)"
-    echo "                        Default: ${install_dir}"
-    echo "    -w <work_dir>      Set the working directory"
-    echo "                        Default: ${work_dir}"
-    echo "    -o <out_dir>       Set the output directory"
-    echo "                        Default: ${out_dir}"
-    echo "    -v                 Enable verbose output"
-    echo "    -h                 This help message"
-    exit ${1}
-}
 
 # Helper function to run make_*() only one time per architecture.
 run_once() {
@@ -227,26 +201,6 @@ if [[ ${EUID} -ne 0 ]]; then
     echo "This script must be run as root."
     _usage 1
 fi
-
-while getopts 'N:V:L:P:A:D:w:o:g:vh' arg; do
-    case "${arg}" in
-        N) iso_name="${OPTARG}" ;;
-        V) iso_version="${OPTARG}" ;;
-        L) iso_label="${OPTARG}" ;;
-        P) iso_publisher="${OPTARG}" ;;
-        A) iso_application="${OPTARG}" ;;
-        D) install_dir="${OPTARG}" ;;
-        w) work_dir="${OPTARG}" ;;
-        o) out_dir="${OPTARG}" ;;
-        g) gpg_key="${OPTARG}" ;;
-        v) verbose="-v" ;;
-        h) _usage 0 ;;
-        *)
-           echo "Invalid argument '${arg}'"
-           _usage 1
-           ;;
-    esac
-done
 
 mkdir -p ${work_dir}
 
